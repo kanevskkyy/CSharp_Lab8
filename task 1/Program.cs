6e9579422641232464328fdab02418fafe317e04
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-14 11:37:17 +0300

    done with task 1

--- task 1/Program.cs
+++ task 1/Program.cs
@@ -7,13 +7,29 @@ class Task
     {
         Console.Write("Information of car {Car} {fuel quaantity} {litters per km} = ");
         string[] carInformation = Console.ReadLine().Split();
-
         PassengerCar car = new PassengerCar(double.Parse(carInformation[1]), double.Parse(carInformation[2]));
 
-        Console.WriteLine("Information of truck {Truck} {fuel quaantity} {litters per km} = ");
+        Console.Write("Information of truck {Truck} {fuel quaantity} {litters per km} = ");
         string[] truckInformation = Console.ReadLine().Split();
-
         Truck truck = new Truck(double.Parse(truckInformation[1]), double.Parse(truckInformation[2]));
+
+        Line();
+        Console.Write("Enter amount of commands = ");
+        int commands = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < commands; i++)
+        {
+            Console.Write($"{i + 1} command = ");
+            string[] information = Console.ReadLine().Split();
+            if (information[0].ToLower() == "drive" && information[1].ToLower() == "car") car.driveCar(double.Parse(information[2]));
+            else if (information[0].ToLower() == "drive" && information[1].ToLower() == "truck") truck.driveCar(double.Parse(information[2]));
+            else if (information[0].ToLower() == "refuel" && information[1].ToLower() == "car") car.addFuel(double.Parse(information[2]));
+            else if (information[0].ToLower() == "refuel" && information[1].ToLower() == "truck") truck.addFuel(double.Parse(information[2]));
+            else Console.WriteLine("Unknown command!");
+        }
+        Line();
+        Console.WriteLine($"Car : {car.fuelAmout:F2}");
+        Console.WriteLine($"Truck : {truck.fuelAmout:F2}");
 
     }
     public static void Line()
