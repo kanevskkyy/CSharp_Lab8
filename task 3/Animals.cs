1c89cb7bf4bb6897396a39964e0e50560473f120
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-14 13:50:28 +0300

    create abstract class Animals and other class from him

---
+++ task 3/Animals.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    abstract class Animal
+    {
+        public string Name { get; }
+        public double Weight { get; protected set; }
+        public int FoodEaten { get; protected set; }
+
+        protected Animal(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+            FoodEaten = 0;
+        }
+
+        public abstract void MakeSound();
+        public abstract void Eat(Food food);
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [{Name}, {Weight}, {FoodEaten}]";
+        }
+    }
+
+    abstract class Bird : Animal
+    {
+        public double WingSize { get; }
+
+        protected Bird(string name, double weight, double wingSize) : base(name, weight)
+        {
+            WingSize = wingSize;
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [{Name}, {WingSize}, {Weight}, {FoodEaten}]";
+        }
+    }
+
+    class Owl : Bird
+    {
+        public Owl(string name, double weight, double wingSize) : base(name, weight, wingSize) { }
+        public override void MakeSound()
+        {
+            Console.WriteLine("Hoot Hoot");
+        }
+
+        public override void Eat(Food food)
+        {
+            if (food is Meat)
+            {
+                FoodEaten += food.Quantity;
+                Weight += 0.25;
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} does not eat {food.GetType().Name}!");
+            }
+        }
+    }
+
+    class Hen : Bird
+    {
+        public Hen(string name, double weight, double wingSize) : base(name, weight, wingSize) { }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Cluck");
+        }
+
+        public override void Eat(Food food)
+        {
+            FoodEaten += food.Quantity;
+            Weight += 0.35;
+        }
+    }
+
+    abstract class Mammal : Animal
+    {
+        public string LivingRegion { get; }
+
+        protected Mammal(string name, double weight, string livingRegion) : base(name, weight)
+        {
+            LivingRegion = livingRegion;
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [{Name}, {Weight}, {LivingRegion}, {FoodEaten}]";
+        }
+    }
+
+    class Mouse : Mammal
+    {
+        public Mouse(string name, double weight, string livingRegion) : base(name, weight, livingRegion) { }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Squeak");
+        }
+
+        public override void Eat(Food food)
+        {
+            if (food is Vegetable || food is Fruit)
+            {
+                FoodEaten += food.Quantity;
+                Weight += 0.10;
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} does not eat {food.GetType().Name}!");
+            }
+        }
+    }
+
+    class Dog : Mammal
+    {
+        public Dog(string name, double weight, string livingRegion) : base(name, weight, livingRegion) { }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Woof!");
+        }
+
+        public override void Eat(Food food)
+        {
+            if (food is Meat)
+            {
+                FoodEaten += food.Quantity;
+                Weight += 0.40;
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} does not eat {food.GetType().Name}!");
+            }
+        }
+    }
+
+    abstract class Feline : Mammal
+    {
+        public string Breed { get; }
+
+        protected Feline(string name, double weight, string livingRegion, string breed)
+            : base(name, weight, livingRegion)
+        {
+            Breed = breed;
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [{Name}, {Breed}, {Weight}, {LivingRegion}, {FoodEaten}]";
+        }
+    }
+
+    class Cat : Feline
+    {
+        public Cat(string name, double weight, string livingRegion, string breed)
+            : base(name, weight, livingRegion, breed) { }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+
+        public override void Eat(Food food)
+        {
+            if (food is Vegetable || food is Meat)
+            {
+                FoodEaten += food.Quantity;
+                Weight += 0.30;
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} does not eat {food.GetType().Name}!");
+            }
+        }
+    }
+
+    class Tiger : Feline
+    {
+        public Tiger(string name, double weight, string livingRegion, string breed)
+            : base(name, weight, livingRegion, breed) { }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("ROAR!!!");
+        }
+
+        public override void Eat(Food food)
+        {
+            if (food is Meat)
+            {
+                FoodEaten += food.Quantity;
+                Weight += 1.00;
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} does not eat {food.GetType().Name}!");
+            }
+        }
+    }
+}
