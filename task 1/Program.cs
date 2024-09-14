b1afed8c1c354f19dff9a08c8a4a5d9b76d01047
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-14 11:27:01 +0300

    create object truck and car

--- task 1/Program.cs
+++ task 1/Program.cs
@@ -5,11 +5,15 @@ class Task
 {
     static void Main()
     {
-        Console.Write("Information of car = ");
+        Console.Write("Information of car {Car} {fuel quaantity} {litters per km} = ");
         string[] carInformation = Console.ReadLine().Split();
 
         PassengerCar car = new PassengerCar(double.Parse(carInformation[1]), double.Parse(carInformation[2]));
-        
+
+        Console.WriteLine("Information of truck {Truck} {fuel quaantity} {litters per km} = ");
+        string[] truckInformation = Console.ReadLine().Split();
+
+        Truck truck = new Truck(double.Parse(truckInformation[1]), double.Parse(truckInformation[2]));
 
     }
     public static void Line()
